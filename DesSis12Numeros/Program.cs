43355913885c37f7be294fbe8a983667ad9a2be4
Usuario <Usuario@CPD02-M04>
2024-05-07 12:21:47 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis12Numeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis12Numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite a quantidade de numeros que deseja digitar: ");
+            int x = int.Parse(Console.ReadLine());
+            int[] xis = new int[x];
+            for (int i <= x) ;
+            {
+                Console.WriteLine("Digite um numero: ");
+                double NumFor[] = new double[j];
+            }
+            
+            
+            
+            
+            
+            Console.ReadKey()
+
+        }
+    }
+}
